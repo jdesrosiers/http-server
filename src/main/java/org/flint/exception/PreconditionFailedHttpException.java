d6823615429f6707e29e611d12bbd50ef008bb91
Jason Desrosiers <jdesrosiers@8thlight.com>
2017-05-04 18:18:32 -0700

    Add a constructor to HttpException to pass cause

--- src/main/java/org/flint/exception/PreconditionFailedHttpException.java
+++ src/main/java/org/flint/exception/PreconditionFailedHttpException.java
@@ -18,4 +18,8 @@ public class PreconditionFailedHttpException extends HttpException {
     public PreconditionFailedHttpException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(StatusCode.PRECONDITION_FAILED, message, cause, enableSuppression, writableStackTrace);
     }
+
+    public PreconditionFailedHttpException(Throwable cause) {
+        super(StatusCode.PRECONDITION_FAILED, cause);
+    }
 }
