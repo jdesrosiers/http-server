aedae895ed48c73e6cb06e24504efc348065003b
Jason Desrosiers <jdesrosiers@8thlight.com>
2017-05-24 13:38:05 -0700

    Add improved wildcard support for URI Templates
    This is still very rudimentary, but it gets me what I need for now

--- src/main/java/org/flint/routing/UriTemplate.java
+++ src/main/java/org/flint/routing/UriTemplate.java
@@ -1,19 +1,27 @@
 package org.flint.routing;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 import javaslang.control.Option;
 import javaslang.collection.HashMap;
+import javaslang.collection.List;
 import javaslang.collection.Map;
 
 public class UriTemplate {
     private String uriTemplate;
 
     public UriTemplate(String uriTemplate) {
-        this.uriTemplate = uriTemplate;
+        this.uriTemplate = templateToPattern(uriTemplate);
+    }
+
+    private String templateToPattern(String uriTemplate) {
+        return "\\Q" + uriTemplate.replaceAll("\\*", "\\\\E.*?\\\\Q") + "\\E";
     }
 
     public Option<Map<String, String>> getMatchFor(String uri) {
-        if (uriTemplate.equals("*") || uri.equals(uriTemplate)) {
-            Map<String, String> matches = HashMap.of();
+        if (Pattern.matches(uriTemplate, uri)) {
+            Map<String, String> matches = HashMap.empty();
             return Option.of(matches);
         } else {
             return Option.none();
--- src/test/java/org/flint/routing/UriTemplateTest.java
+++ src/test/java/org/flint/routing/UriTemplateTest.java
@@ -4,28 +4,44 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 
 import javaslang.collection.HashMap;
 import javaslang.control.Option;
 
+@RunWith(DataProviderRunner.class)
 public class UriTemplateTest {
 
     @Test
-    public void testSimpleMatch() {
+    public void itShouldMatchOnAnExactMatch() {
         UriTemplate uriTemplate = new UriTemplate("/foo");
         assertThat(uriTemplate.getMatchFor("/foo"), equalTo(Option.of(HashMap.of())));
     }
 
     @Test
-    public void testSimpleFail() {
+    public void itShouldFailIfItDoesntMatch() {
         UriTemplate uriTemplate = new UriTemplate("/foo");
         assertThat(uriTemplate.getMatchFor("/bar"), equalTo(Option.none()));
     }
 
+    @DataProvider
+    public static Object[][] dataProviderWildcardTemplates() {
+        return new Object[][] {
+            { "*", "/bar" },
+            { "/foo/*", "/foo/bar" },
+            { "*.json", "/bar.json" },
+            { "/foo/*.json", "/foo/bar.json" }
+        };
+    }
+
     @Test
-    public void testWildcardMatch() {
-        UriTemplate uriTemplate = new UriTemplate("*");
-        assertThat(uriTemplate.getMatchFor("/bar"), equalTo(Option.of(HashMap.of())));
+    @UseDataProvider("dataProviderWildcardTemplates")
+    public void itShouldMatchWildcardExperessions(String template, String subject) {
+        UriTemplate uriTemplate = new UriTemplate(template);
+        assertThat(uriTemplate.getMatchFor(subject), equalTo(Option.of(HashMap.of())));
     }
 
 }
